32ec7a4876d947f01f048567c000f133da78e60c
Codedbyte <sanizayyad99@gmail.com>
2018-11-09 02:07:22 +0300

    background color changed

--- lib/main.dart
+++ lib/main.dart
@@ -37,6 +37,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
+      backgroundColor: Colors.white70.withAlpha(220),
         appBar: new AppBar(
           title: new Text('Pokemon'),
           backgroundColor: Colors.cyan,
